b77e061096fc63436457d172fd9b67e85482cd4e
paolozu <adam.paolo@gmail.com>
2018-08-17 17:53:15 +0200

    Finish Model

---
+++ Server/src/main/java/Model/Bean/Tutore.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Bean;
+
+/**
+ *
+ * @author Paolo
+ */
+public class Tutore extends Utente {
+    
+    private String nome, cognome, telefono;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+}
