fb1e0a2b96a7292576fd91e67d2d162dd8f8477c
Lorenzo Andreoli <lorenzo.andreoli@student.univaq.it>
2018-08-17 17:13:51 +0200

    ER 2.0

--- Server/src/main/java/Model/Bean/Studente.java
+++ Server/src/main/java/Model/Bean/Studente.java
@@ -3,59 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model.Bean;
 
+package Model.Bean;
+import java.util.Date;
 /**
  *
  * @author lorenzo
  */
 public class Studente extends Utente {
     
-    private String nome,cognome,codFiscale,telefono;
-    boolean handicap;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public String getCodFiscale() {
-        return codFiscale;
-    }
-
-    public void setCodFiscale(String codFiscale) {
-        this.codFiscale = codFiscale;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public boolean isHandicap() {
-        return handicap;
-    }
-
-    public void setHandicap(boolean handicap) {
-        this.handicap = handicap;
-    }
+    private String nome, cognome, codFiscale, telefono, crediti;
+    Date dataNascita;
+    private String indirizzoResidenza, corsoLaurea, diploma,laurea,dottorato, settore;
+    private boolean handicap;
+    private int capNascita,capResidenza;
+    private String cittaResidenza,provinciaResidenza, cittaNascita, provinciaNascita;
     
+
+
     
-    
-    
-    
+  
 }
--- Server/src/main/java/Model/DB.java
+++ Server/src/main/java/Model/DB.java
@@ -65,5 +65,4 @@ public class DB {
         }
         return conn;
     }
-
 }
