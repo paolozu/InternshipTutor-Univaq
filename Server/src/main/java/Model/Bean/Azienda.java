a79e5e32a9ba2fbf99ba7a12aed9557af7b0b653
paolozu <adam.paolo@gmail.com>
2018-08-17 17:10:02 +0200

    Model Azienda

---
+++ Server/src/main/java/Model/Bean/Azienda.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Bean;
+
+/**
+ *
+ * @author Paolo
+ */
+public class Azienda extends Utente {
+    
+    private int id, CAP, idConvenzione;
+    private String nomeRappresentante, cognomeRappresentante, ragioneSociale,
+            indirizzoSede, partitaIva, foroCompetente, telefono;
+    boolean abilitata;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCAP() {
+        return CAP;
+    }
+
+    public int getIdConvenzione() {
+        return idConvenzione;
+    }
+
+    public String getNomeRappresentante() {
+        return nomeRappresentante;
+    }
+
+    public String getCognomeRappresentante() {
+        return cognomeRappresentante;
+    }
+
+    public String getRagioneSociale() {
+        return ragioneSociale;
+    }
+
+    public String getIndirizzoSede() {
+        return indirizzoSede;
+    }
+
+    public String getPartitaIva() {
+        return partitaIva;
+    }
+
+    public String getForoCompetente() {
+        return foroCompetente;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public boolean isAbilitata() {
+        return abilitata;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCAP(int CAP) {
+        this.CAP = CAP;
+    }
+
+    public void setIdConvenzione(int idConvenzione) {
+        this.idConvenzione = idConvenzione;
+    }
+
+    public void setNomeRappresentante(String nomeRappresentante) {
+        this.nomeRappresentante = nomeRappresentante;
+    }
+
+    public void setCognomeRappresentante(String cognomeRappresentante) {
+        this.cognomeRappresentante = cognomeRappresentante;
+    }
+
+    public void setRagioneSociale(String ragioneSociale) {
+        this.ragioneSociale = ragioneSociale;
+    }
+
+    public void setIndirizzoSede(String indirizzoSede) {
+        this.indirizzoSede = indirizzoSede;
+    }
+
+    public void setPartitaIva(String partitaIva) {
+        this.partitaIva = partitaIva;
+    }
+
+    public void setForoCompetente(String foroCompetente) {
+        this.foroCompetente = foroCompetente;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setAbilitata(boolean abilitata) {
+        this.abilitata = abilitata;
+    }
+    
+    
+}
